0d6e6119876f49d9fc33b418861adbdb6c7c4d13
Alexander Rajula <alexander@rajula.org>
2011-11-27 18:04:30 +0100

    Changed names to something more useful.

--- code/matlabplot.m
+++ code/matlabplot.m
@@ -1,10 +1,10 @@
 LogData = importdata('logfile.dat');
 Time = LogData.data(:,1);
 Window = LogData.data(:,2);
-Byterate = LogData.data(:,3);
-Throughput = LogData.data(:,4);
-InstantByterate = LogData.data(:,5);
-InstantThroughput = LogData.data(:,6);
+OutputByterate = LogData.data(:,3);
+OutputDataByterate = LogData.data(:,4);
+OutputInstantByterate = LogData.data(:,5);
+OutputInstantThroughput = LogData.data(:,6);
 
 figure('Name', 'Window size', 'NumberTitle', 'off');
 plot(Time, Window);
@@ -19,4 +19,14 @@ ylabel('Byte rate (raw output)');
 figure('Name', 'Throughput', 'NumberTitle', 'off');
 plot(Time, Throughput);
 xlabel('Time (s)');
-ylabel('Throughput (data output)');
+ylabel('Data rate (raw data output)');
+
+figure('Name', 'Instant byte rate', 'NumberTitle', 'off');
+plot(Time, InstantByterate);
+xlabel('Time (s)');
+ylabel('Instant byterate (raw output)');
+
+figure('Name', 'Instant throughput', 'NumberTitle', 'off');
+plot(Time, InstantThroughput);
+xlabel('Time (s)');
+ylabel('Instant data rate (raw data output)');
